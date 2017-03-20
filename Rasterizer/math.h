afa9807611c04bc905e421c47f427b68d56fcb4f
huang <961437466@qq.com>
2017-03-20 15:44:44 +0800

    modify math

--- Rasterizer/math.h
+++ Rasterizer/math.h
@@ -4,4 +4,17 @@
 #include "vec4.h"
 #include "mat4.h"
 
+#include <algorithm>
+
+Mat4 transpose(Mat4 m) {
+	using std::swap;
+	swap(m[0][1], m[1][0]);
+	swap(m[0][2], m[2][0]);
+	swap(m[0][3], m[3][0]);
+	swap(m[1][2], m[2][1]);
+	swap(m[1][3], m[3][1]);
+	swap(m[2][3], m[3][2]);
+	return m;
+}
+
 #endif // !MATH_H
--- Rasterizer/vec4.cpp
+++ Rasterizer/vec4.cpp
@@ -1,26 +1,20 @@
 #include "vec4.h"
 #include "mat4.h"
-#include <initializer_list>
-#include <algorithm>
-
-
-
-Vec4::Vec4(std::initializer_list<float> lst) : Vec4() {
-	float *item = reinterpret_cast<float*>(this);
-	std::for_each(lst.begin(), lst.end(), [&item](float f) { *item++ = f; });
-}
-
 
 
 Vec4 Vec4::operator*(const Mat4 &M) const {
 	Vec4 v;
-	float *item = reinterpret_cast<float*>(&v);
 
 	for (std::size_t i = 0; i < 4; ++i) {
-		*item++ = this->x_ * M[0][i] + this->y_ * M[1][i] + this->z_ * M[2][i] + this->w_ * M[3][i];
+		v.arr[i] = this->arr[0] * M[0][i] + this->arr[i] * M[1][i] + this->arr[2] * M[2][i] + this->arr[3] * M[3][i];
 	}
+
+	// May use NRVO.
+	// If not£¬call Move Copy constructor of Vec4
 	return v;
 }
+
+
 
 
 
