89d4b2fbdd9b7e7114c507d6a9f59610b641467c
huang <961437466@qq.com>
2017-03-20 15:53:10 +0800

    modify math & fix file name

--- Rasterizer/mat4.h
+++ Rasterizer/mat4.h
@@ -2,12 +2,17 @@
 #define MAT4_H
 
 #include <array>
+#include <ostream>
+#include <initializer_list>
 #include "vec4.h"
 using std::array;
 
 class Mat4 {
+	friend std::ostream &operator<<(std::ostream &out, const Mat4 &m);
 public:
 	Mat4();
+	Mat4(const std::initializer_list<float> &lst);
+	Mat4(std::initializer_list<float> &&lst);
 	Mat4(array<Vec4, 4> &&vec4s);
 	Mat4(const array<Vec4, 4> &vec4s);
 
@@ -21,23 +26,43 @@ public:
 
 	Vec4 &operator[](size_t i);
 	const Vec4 &operator[](size_t i) const;
+
 	Mat4 operator*(const Mat4 &m) const;
+	Vec4_T operator*(const Vec4_T &v) const;
+	Vec4 operator*(const Vec4 &) const = delete;
 
 private:
 	array<Vec4, 4> m;
 };
 
-#endif // !MAT4_H
+
+
 
 
 
 
 
 inline Mat4::Mat4()
-	: m{ Vec4(1.0f, 0.0f, 0.0f, 0.0f),
-	     Vec4(0.0f, 1.0f, 0.0f, 0.0f),
-	     Vec4(0.0f, 0.0f, 1.0f, 0.0f),
-	     Vec4(0.0f, 0.0f, 0.0f, 1.0f) } {}
+	: Mat4{ 1.0f, 0.0f, 0.0f, 0.0f,
+		    0.0f, 1.0f, 0.0f, 0.0f,
+		    0.0f, 0.0f, 1.0f, 0.0f,
+		    0.0f, 0.0f, 0.0f, 1.0f } {}
+
+
+inline Mat4::Mat4(const std::initializer_list<float> &lst)
+	: m{ Vec4( *lst.begin(), *(lst.begin() + 1), *(lst.begin() + 2), *(lst.begin() + 3) ),
+	     Vec4( *(lst.begin() + 4), *(lst.begin() + 5), *(lst.begin() + 6),*(lst.begin() + 7) ),
+	     Vec4( *(lst.begin() + 8), *(lst.begin() + 9), *(lst.begin() + 10), *(lst.begin() + 11) ),
+	     Vec4( *(lst.begin() + 12), *(lst.begin() + 13), *(lst.begin() + 14), *(lst.begin() + 15) ) } {}
+
+
+inline Mat4::Mat4(std::initializer_list<float> &&lst)
+	: m{ Vec4(*lst.begin(), *(lst.begin() + 1), *(lst.begin() + 2), *(lst.begin() + 3)),
+		 Vec4(*(lst.begin() + 4), *(lst.begin() + 5), *(lst.begin() + 6),*(lst.begin() + 7)),
+		 Vec4(*(lst.begin() + 8), *(lst.begin() + 9), *(lst.begin() + 10), *(lst.begin() + 11)),
+		 Vec4(*(lst.begin() + 12), *(lst.begin() + 13), *(lst.begin() + 14), *(lst.begin() + 15)) } {}
+
+
 
 inline Mat4::Mat4(array<Vec4, 4> &&vec4s) : m(vec4s) {}
 
@@ -80,6 +105,30 @@ inline Mat4 Mat4::operator*(const Mat4 &m) const {
 		}
 		rlt[i] = v;
 	}
+	// NRVO or call move copy constructor
 	return rlt;
 }
 
+
+inline Vec4_T Mat4::operator*(const Vec4_T &v) const {
+	Vec4_T rlt;
+	for (size_t i = 0; i < 4; ++i) {
+		rlt[i] = (*this)[i][0] * v[0] + (*this)[i][1] * v[1] + (*this)[i][2] * v[2] + (*this)[i][3] * v[3];
+	}
+	// NRVO or call move copy constructor
+	return rlt;
+}
+
+
+
+inline std::ostream &operator<<(std::ostream &out, const Mat4 &m) {
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j)
+			out << m[i][j] << "  ";
+		out << "\n";
+	}
+	return out;
+}
+
+
+#endif // !MAT4_H
--- Rasterizer/vec4.h
+++ Rasterizer/vec4.h
@@ -4,93 +4,135 @@
 
 #include "point.h"
 #include <array>
+#include <ostream>
 #include <initializer_list>
+#include <utility>
 using std::array;
 using std::size_t;
 
 
 class Mat4;
+class Vec4;
+class Vec4_T;
 
-class Vec4 final {
+
+class Vec4Base {
+	friend Vec4;
+	friend std::ostream &operator<<(std::ostream &out, const Vec4Base &v);
+	friend void swap(Vec4Base &v1, Vec4Base &v2);
 public:
-	Vec4();
-	Vec4(Point3 &p, float w);
-	Vec4(std::initializer_list<float> lst);
-	Vec4(float x, float y, float z, float w);
+	float x() const noexcept { return this->arr[0]; }
+	float y() const noexcept { return this->arr[1]; }
+	float z() const noexcept { return this->arr[2]; }
+	float w() const noexcept { return this->arr[3]; }
 
-	Vec4(const Vec4 &V);
-	Vec4(Vec4 &&V);
-
-	Vec4 &operator=(Vec4 V);
-
-	~Vec4() = default;
-
-	Vec4 operator*(const Mat4 &M) const;
-	
 	float &operator[](std::size_t i);
 	const float operator[](std::size_t i) const;
 
-	float x() const noexcept { return this->x_; }
-	float y() const noexcept { return this->y_; }
-	float z() const noexcept { return this->z_; }
-	float w() const noexcept { return this->w_; }
+protected:
+	Vec4Base();
+	Vec4Base(Point3 &p, float w);
+	Vec4Base(std::initializer_list<float> lst);
+	Vec4Base(float x, float y, float z, float w);
 
+	Vec4Base(const Vec4Base &V);
+	Vec4Base(Vec4Base &&V);
 
-	friend void swap(Vec4 &v1, Vec4 &v2) {
-		using std::swap;
-		if (&v1 == &v2) { return; }
-		swap(v1.x_, v2.x_);
-		swap(v1.y_, v2.y_);
-		swap(v1.z_, v2.z_);
-		swap(v1.w_, v2.w_);
-	}
-private:
-	float x_;
-	float y_;
-	float z_;
-	float w_;
+	Vec4Base &operator=(Vec4Base V);
 
+	// if use pure virtual, argument type ['Vec4Base'] is not allowed. 
+	virtual ~Vec4Base() = default;
 	
+	
+private:
+	array<float, 4> arr;
 };
 
 
-inline Vec4::Vec4() : x_(0.0f), y_(0.0f), z_(0.0f), w_(0.0f) {}
+struct Vec4 final : public Vec4Base {
+	// Perfect forwarding don't work with initializer_list
+	Vec4(std::initializer_list<float> lst) : Vec4Base(lst) {}
+
+	template<typename ... Args>
+	Vec4(Args&& ... args) : Vec4Base(std::forward<Args>(args)...) { }
 
 
-inline Vec4::Vec4(Point3 &p, float w) : x_(p.p1), y_(p.p2), z_(p.p3), w_(w) {}
+	Vec4 operator*(const Mat4 &M) const;
+
+	~Vec4() override {};
+	
+	//float x, y, z, w;
+};
 
 
-inline Vec4::Vec4(float x, float y, float z, float w) : x_(x), y_(y), z_(z), w_(w) {}
+struct Vec4_T final : Vec4Base {
+	Vec4_T(std::initializer_list<float> lst) : Vec4Base(lst) {}
+
+	template<typename ... Args>
+	Vec4_T(Args&& ... args) : Vec4Base(std::forward<Args>(args)...) {}
 
 
-inline Vec4::Vec4(const Vec4 &V) : x_(V.x_), y_(V.y_), z_(V.z_), w_(V.w_) {}
+	~Vec4_T() override {};
+
+	//float x,
+	//      y,
+	//      z,
+	//      w;
+};
 
 
-inline Vec4::Vec4(Vec4 &&V) : Vec4() { swap(*this, V); }
 
-inline Vec4 &Vec4::operator=(Vec4 V) {
+
+
+
+inline Vec4Base::Vec4Base() : arr{ 0.0f, 0.0f, 0.0f, 0.0f } {}
+
+inline Vec4Base::Vec4Base(Point3 &p, float w) : arr{ p.p1, p.p2, p.p3, w } {}
+
+
+inline Vec4Base::Vec4Base(std::initializer_list<float> lst) : Vec4Base() {
+	std::move(lst.begin(), lst.end(), this->arr.begin());
+}
+
+
+inline Vec4Base::Vec4Base(float x, float y, float z, float w) : arr{ x, y, z, w } {}
+
+
+inline Vec4Base::Vec4Base(const Vec4Base &V) : arr(V.arr) {}
+
+
+inline Vec4Base::Vec4Base(Vec4Base &&V) : Vec4Base() { swap(*this, V); }
+
+inline Vec4Base &Vec4Base::operator=(Vec4Base V) {
 	swap(*this, V);
 	return *this;
 }
 
 
-inline float &Vec4::operator[](std::size_t i) {
-	if (i < 4) {
-		float *item = reinterpret_cast<float*>(this);
-		return *(item + i);
-	}
+inline float &Vec4Base::operator[](std::size_t i) {
+	return this->arr.at(i);
 }
 
 
-inline const float Vec4::operator[](std::size_t i) const {
-	if (i < 4) {
-		float *item = reinterpret_cast<float*>(const_cast<Vec4*>(this));
-		return *(item + i);
-	}
+inline const float Vec4Base::operator[](std::size_t i) const {
+	return this->arr.at(i);
 }
 
 
+inline std::ostream &operator<<(std::ostream &out, const Vec4Base &v) {
+	std::copy(v.arr.begin(), v.arr.end(), std::ostream_iterator<float>(out, dynamic_cast<const Vec4_T*>(&v) ? " \n" : "  "));
+	return out;
+}
 
+
+inline void swap(Vec4Base &v1, Vec4Base &v2) {
+	using std::swap;
+	if (&v1 == &v2) { return; }
+	swap(v1.arr[0], v2.arr[0]);
+	swap(v1.arr[1], v2.arr[1]);
+	swap(v1.arr[2], v2.arr[2]);
+	swap(v1.arr[3], v2.arr[3]);
+}
 
 
 #endif // !VEC4_H
